cfd0cae9fa0ba5adfbcf46ac083b96847509585b
Amina Ly <aminaly@SE3-C02W3433HV29.local>
2021-03-22 20:12:30 -0700

    combining data

--- combine_shelter.R
+++ combine_shelter.R
@@ -9,7 +9,7 @@ library(lubridate)
 library(reshape2)
 
 ## first lets do the files with all movement data
-all_files <- list.files("heatwaves_manual/safegraph/patterns/2020/12/17", full.names = T, recursive = T, pattern = "*.csv")
+all_files <- list.files("heatwaves_manual/safegraph/patterns/2020/12/17", full.names = T, recursive = T, pattern = "*.csv.gz")
 combined <- data.frame()
 
 for(file in all_files) {
@@ -21,23 +21,18 @@ for(file in all_files) {
   )
   
   if(inherits(possibleError, "error")) next
-  
-  saveRDS(f[1:10000,], "heatwaves_manual/neighborhood_short.rds")
-  
+  f <- f %>% select(census_block_group = area, date = date_range_start, raw_device_counts, distance_from_home,
+                    distance_from_primary_daytime_location, median_dwell, state = region, device_home_areas)
   combined <- bind_rows(combined, f)
   
 }
 
 combined$fips <- str_sub(combined$census_block_group, 1,5)
-sheltering <- combined %>% group_by(fips, date_range_start) %>% 
-  summarise(home_device = sum(completely_home_device_count, na.rm = T), 
-            all_devices = sum(device_count, na.rm = T))
+saveRDS(combined, "heatwaves_manual/patterns_raw_blockgroup.rds")
 
-sheltering$shelter_index <- (sheltering$home_device / sheltering$all_devices)*100
-saveRDS(sheltering, "heatwaves_manual/patterns_raw_blockgroup.rds")
-
-sheltering <- sheltering %>% group_by(fips, date = date_range_start, home_device, all_devices) %>% summarise(shelter_index = mean(shelter_index))
-saveRDS(sheltering, "heatwaves_manual/patterns_raw_fips.rds")
+#waiting until I figure out how to translate their python dicts to R 
+#sheltering <- sheltering %>% group_by(fips, date = date_range_start, home_device, all_devices) %>% summarise(shelter_index = mean(shelter_index))
+#saveRDS(sheltering, "heatwaves_manual/patterns_raw_fips.rds")
 
 #####################
 #### now lets combine the actual neighborhood devices residing patterns by day
