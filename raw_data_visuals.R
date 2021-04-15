f8e28914283571411d2699986ca944c0fd5b50dd
Amina Ly <amina.ly626@gmail.com>
2021-04-14 19:30:33 -0700

    fixed visual

--- raw_data_visuals.R
+++ raw_data_visuals.R
@@ -104,7 +104,7 @@ i_m_short %>%
   geom_bar(data=i_m_short, aes(x=income_group, y = n),
            stat="identity",
            position='dodge') +
-  theme_bw() + ylab("Count")
+  theme_bw() + ylab("Count") +
   facet_wrap( ~ region_s, scales = "free") + 
   #scale_y_continuous(expand = c(0, 2), limits = c(0, NA)) +
   #scale_x_continuous(expand = c(0, 0), limits = c(0, NA)) +
@@ -144,7 +144,7 @@ ggplot(data=data_sub, aes(x=date, y=yvar, group=income_group)) +
   geom_smooth(aes(group = income_group, colour=as.factor(income_group))) +
   ggtitle("Mobility Change 2020 - 2019") + ylab("% Sheltering") + xlab("Date") +
   scale_color_manual(values=wes_palette(n=5, name="Zissou1")) +
- # facet_wrap( ~ region_s, scales = "free", nrow = 2) +
+  facet_wrap( ~ region_s, scales = "free", nrow = 2) +
   scale_x_date() +
   theme(text = element_text(size = 15)) +
   labs(colour="Income Grp (5 High)") +
