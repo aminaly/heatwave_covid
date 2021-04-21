e495139d81f67640ff0337492ad7c04342b9f0cc
Amina Ly <aminaly@SE3-C02W3433HV29.local>
2021-04-20 17:15:56 -0700

    bash file for clean shelter

---
+++ run_clean_shelter.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=clean_shelter
+#SBATCH --nodes=1
+#SBATCH --error=/oak/stanford/groups/omramom/group_members/aminaly/heatwave_covid/outfiles/clean_shelter.err
+#SBATCH --output=/oak/stanford/groups/omramom/group_members/aminaly/heatwave_covid/outfiles/clean_shelter.out
+#SBATCH --time=10:00:00
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem-per-cpu=100GB
+#SBATCH --mail-type=END,FAIL 
+#SBATCH --mail-user=aminaly@stanford.edu
+#SBATCH -p diffenbaugh
+
+ml physics gdal udunits/2.2.26 netcdf/4.4.1.1 R/3.6.1 proj geos;
+module load R/3.6.1
+
+cd $OAK/group_members/aminaly/heatwave_covid
+Rscript ./clean_shelter.R
+
