02dfb3817302c2de5387c21f941cbac9e1320ab3
Amina Ly <aminaly@SE3-C02W3433HV29.local>
2021-04-22 17:43:16 -0700

    type compatibility

--- clean_shelter.R
+++ clean_shelter.R
@@ -17,11 +17,14 @@ d <- expand_integer_json(p, "stops_by_day", index = "day",  by = c("census_block
 
 #rename for clarity
 d$date <- d$date + days(d$day - 1) 
+d <- d %>% mutate(month = month(date)) %>% mutate(year = year(date))
 
 # join with the primary residence table so we know how many live there
 r <- readRDS(paste0(getwd(), "/heatwaves_manual/all_residing_raw_blockgroup.rds"))
+r <- r %>% filter(fips %in% unique(d$fips))
 
-patterns_clean_blockgroup <- left_join(d, r, by = c("census_block_group", "month", "year", "state"))
+patterns_clean_blockgroup <- left_join(d, r, by = c("census_block_group", "month", "year", "fips"))
+patterns_clean_blockgroup <- patterns_clean_blockgroup %>% rename(state = state.x)
 
 patterns_clean_blockgroup$visitors = patterns_clean_blockgroup$stops_by_day - patterns_clean_blockgroup$number_devices_residing
 patterns_clean_blockgroup$visitors_percap = patterns_clean_blockgroup$visitors / patterns_clean_blockgroup$number_devices_residing
