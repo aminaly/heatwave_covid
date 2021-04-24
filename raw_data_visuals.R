7ab8cee6474cfe092f01980a8a0743f55c77b43b
Amina Ly <aminaly@SE3-C02W3433HV29.local>
2021-04-23 19:03:49 -0700

    getting rid of the last visual bc it keeps breaking

--- raw_data_visuals.R
+++ raw_data_visuals.R
@@ -127,18 +127,18 @@ ggplot(data=data, aes(x=date, y=p_high, group=year)) +
   theme_bw()
 
 # loess difference of 2020 - 2019 mobility
-data_sub <- left_join(data2019, data2020, by = c("fips", "month.x", "day"))
-data_sub <- data_sub %>% mutate(yvar = yvar.y - yvar.x) %>% rename(income_group = income_group.x) %>%
-  rename(date = date.x) %>% rename(region_s = region_s.x)
-
-ggplot(data=data_sub, aes(x=date, y=yvar, group=income_group)) +
-  geom_smooth(aes(group = income_group, colour=as.factor(income_group))) +
-  ggtitle("Mobility Change 2020 - 2019") + ylab("% Sheltering") + xlab("Date") +
-  scale_color_manual(values=wes_palette(n=5, name="Zissou1")) +
-  scale_x_date() +
-  theme(text = element_text(size = 15)) +
-  labs(colour="Income Grp (5 High)") +
-  theme_bw()
+# data_sub <- left_join(data2019, data2020, by = c("census_block_group", "month.x", "day"))
+# data_sub <- data_sub %>% mutate(yvar = yvar.y - yvar.x) %>% rename(income_group = income_group.x) %>%
+#   rename(date = date.x) %>% rename(region_s = region_s.x)
+# 
+# ggplot(data=data_sub, aes(x=date, y=yvar, group=income_group)) +
+#   geom_smooth(aes(group = income_group, colour=as.factor(income_group))) +
+#   ggtitle("Mobility Change 2020 - 2019") + ylab("% Sheltering") + xlab("Date") +
+#   scale_color_manual(values=wes_palette(n=5, name="Zissou1")) +
+#   scale_x_date() +
+#   theme(text = element_text(size = 15)) +
+#   labs(colour="Income Grp (5 High)") +
+#   theme_bw()
 
 
 
