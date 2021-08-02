9d09b636cc5f431ffbf738e4cc3c8ecf7cde7a42
Amina Ly <aminaly@SE3-C02W3433HV29.local>
2021-08-02 16:58:15 -0400

    I had accidenally deleted some lines while editing

--- figure3.R
+++ figure3.R
@@ -22,7 +22,7 @@ library(interactions)
 data <- readRDS("./heatwaves_manual/data_for_regression.rds")
 
 ## lets do some plots
-pdf(paste0("./visuals/figure3_", Sys.Date(), ".pdf"))
+pdf(paste0("./visuals/figure3", Sys.Date(), ".pdf"))
 ##Finalize datasets for regressions & run
 plot(c(0, 1), c(0, 1), ann = F, bty = 'n', type = 'n', xaxt = 'n', yaxt = 'n',
      main = title)
@@ -36,14 +36,48 @@ plot_data_bin <- function(data, plot_title, xlab="Temp (C)", ylab = "# Visitors 
   BINS <- 9
   data <- data %>% mutate(xvar_bin = cut(xvar, BINS, labels = F)) %>% 
     mutate(xvar_bin = factor(xvar_bin, levels = as.character(1:BINS)))
-  
+
   #separate the two datasets
   data_1 <- data %>% filter(year %in% c(2018,2019))
   data_2 <- data %>% filter(year == 2020)
-
+  
   #get the results of the model
   model_1 <- fe_model(data_1, level = LVL)
   model_2 <- fe_model(data_2, level = LVL)
+  
+  #bootstrap the data by doing the model however many times, getting the coefficient values (1)
+  boots_1 <- bootstrap_data(data_1, short=T, level= LVL)
+  boots_1 <- boots_1[-1,]
+  boots_1 <- as_tibble(boots_1) %>% mutate(xvar_bin1 = 0) %>% relocate(xvar_bin1)
+  conf_1 <- apply(boots_1,2,function(x) quantile(x,probs=c(0.05,0.5,0.95), na.rm = T)) 
+  conf_1 <- as.data.frame(cbind(1:BINS, conf_1[1,], conf_1[2,], conf_1[3,]))
+  colnames(conf_1) <- c("term", "low", "mid", "upper")   
+  conf_1$term <- row.names(conf_1)
+  
+  coefs_1 <- tidy(model_1, conf.int = T)
+  coefs_1 <- coefs_1[grepl("xvar_bin", coefs_1$term),]
+  coefs1_1 <- coefs_1[1,] %>% mutate(term = "xvar_bin1", estimate = 0, std.error = 0, statistic = 0, p.value = 0, conf.low = 0, conf.high = 0)
+  coefs_1 <- rbind(coefs1_1, coefs_1)
+  coefs_1$grp <- "2018-19"
+  coefs_1 <- left_join(coefs_1, conf_1, by = "term")
+  
+  #bootstrap the data by doing the model however many times, getting the coefficient values (2)
+  boots_2 <- bootstrap_data(data_2, short=T, level= LVL)
+  boots_2 <- boots_2[-1,]
+  boots_2 <- as_tibble(boots_2) %>% mutate(xvar_bin1 = 0) %>% relocate(xvar_bin1)
+  conf_2 <- apply(boots_2,2,function(x) quantile(x,probs=c(0.05,0.5,0.95), na.rm = T)) 
+  conf_2 <- as.data.frame(cbind(1:BINS, conf_2[1,], conf_2[2,], conf_2[3,]))
+  colnames(conf_2) <- c("term", "low", "mid", "upper")   
+  conf_2$term <- row.names(conf_2)
+  
+  coefs_2 <- tidy(model_2, conf.int = T)
+  coefs_2 <- coefs_2[grepl("xvar_bin", coefs_2$term),]
+  coefs1_2 <- coefs_2[1,] %>% mutate(term = "xvar_bin1", estimate = 0, std.error = 0, statistic = 0, p.value = 0, conf.low = 0, conf.high = 0)
+  coefs_2 <- rbind(coefs1_2, coefs_2)
+  coefs_2$grp <- "2020"
+  coefs_2 <- left_join(coefs_2, conf_2, by = "term")
+  
+  coefs <- rbind(coefs_1, coefs_2)
   
   ## plot both the 2018-19 and 2020 data
   ggplot(data = coefs, aes(term, estimate, group = grp))+
