83f43576e2f3bfbf36a6c2b94ba8b635f189d6a4
Amina Ly <aminaly@SE3-C02W3433HV29.local>
2021-08-04 23:35:50 -0400

    fixed naming on figure files

---
+++ figure2_.R
@@ -0,0 +1,113 @@
+ifelse(dir.exists("~/Box Sync/heatwave_covid"),
+       setwd("~/Box Sync/heatwave_covid"),
+       setwd("/oak/stanford/groups/omramom/group_members/aminaly/heatwave_covid"))
+
+source("./regression_functions_shelter.R")
+
+library(dplyr)
+library(lfe)
+library(ggplot2)
+library(dotwhisker)
+library(tidyverse)
+library(lubridate)
+library(reshape2)
+library(stringr)
+library(broom)
+library(gridExtra)
+library(wesanderson)
+library(SafeGraphR)
+library(interactions)
+
+## read in the regression data
+data <- readRDS("./heatwaves_manual/data_for_regression.rds")
+
+## lets do some plots
+pdf(paste0("./visuals/pub_figures/fig2_", Sys.Date(), ".pdf"))
+##Finalize datasets for regressions & run
+plot(c(0, 1), c(0, 1), ann = F, bty = 'n', type = 'n', xaxt = 'n', yaxt = 'n',
+     main = title)
+text(x = 0.5, y = 0.5, paste(timestamp(), "\n Bay Area Data Overview"),
+     cex = 1.5, col = "black")
+
+plot_data_bin <- function(data, plot_title, xlab="Temp (C)", ylab = "# Visitors / Home Devices", summer = F) {
+  
+  #create the bins
+  LVL <- "bin"
+  if(summer) {
+    BINS <- 6
+    data_s <- data %>% filter(between(month.x, 5, 9))
+    data_s <- data_s %>% mutate(xvar_bin = cut(xvar, breaks = c(-Inf, seq(16, 35, 4), Inf), labels = F)) %>% 
+      mutate(xvar_bin = factor(xvar_bin, levels = as.character(1:BINS))) %>% filter(!is.na(xvar_bin))
+  } else {
+    BINS <- 8
+    data_s <- data %>% mutate(xvar_bin = cut(xvar, breaks = c(-Inf, seq(10, 35, 4), Inf), labels = F)) %>% 
+    mutate(xvar_bin = factor(xvar_bin, levels = as.character(1:BINS))) %>% filter(!is.na(xvar_bin))
+  }
+  
+  #separate the two datasets
+  data_1 <- data_s %>% filter(year %in% c(2018,2019))
+  data_2 <- data_s %>% filter(year == 2020)
+  
+  #get the results of the model
+  model_1 <- fe_model(data_1, level = LVL)
+  model_2 <- fe_model(data_2, level = LVL)
+  
+  #bootstrap the data by doing the model however many times, getting the coefficient values (1)
+  boots_1 <- bootstrap_data(data_1, short=T, level= LVL)
+  boots_1 <- boots_1[-1,]
+  boots_1 <- as_tibble(boots_1) %>% mutate(xvar_bin1 = 0) %>% relocate(xvar_bin1)
+  conf_1 <- apply(boots_1,2,function(x) quantile(x,probs=c(0.05,0.5,0.95), na.rm = T)) 
+  conf_1 <- as.data.frame(cbind(1:BINS, conf_1[1,], conf_1[2,], conf_1[3,]))
+  colnames(conf_1) <- c("term", "low", "mid", "upper")   
+  conf_1$term <- row.names(conf_1)
+  
+  coefs_1 <- tidy(model_1, conf.int = T)
+  coefs_1 <- coefs_1[grepl("xvar_bin", coefs_1$term),]
+  coefs1_1 <- coefs_1[1,] %>% mutate(term = "xvar_bin1", estimate = 0, std.error = 0, statistic = 0, p.value = 0, conf.low = 0, conf.high = 0)
+  coefs_1 <- rbind(coefs1_1, coefs_1)
+  coefs_1$grp <- "2018-19"
+  coefs_1 <- left_join(coefs_1, conf_1, by = "term")
+  
+  #bootstrap the data by doing the model however many times, getting the coefficient values (2)
+  boots_2 <- bootstrap_data(data_2, short=T, level= LVL)
+  boots_2 <- boots_2[-1,]
+  boots_2 <- as_tibble(boots_2) %>% mutate(xvar_bin1 = 0) %>% relocate(xvar_bin1)
+  conf_2 <- apply(boots_2,2,function(x) quantile(x,probs=c(0.05,0.5,0.95), na.rm = T)) 
+  conf_2 <- as.data.frame(cbind(1:BINS, conf_2[1,], conf_2[2,], conf_2[3,]))
+  colnames(conf_2) <- c("term", "low", "mid", "upper")   
+  conf_2$term <- row.names(conf_2)
+  
+  coefs_2 <- tidy(model_2, conf.int = T)
+  coefs_2 <- coefs_2[grepl("xvar_bin", coefs_2$term),]
+  coefs1_2 <- coefs_2[1,] %>% mutate(term = "xvar_bin1", estimate = 0, std.error = 0, statistic = 0, p.value = 0, conf.low = 0, conf.high = 0)
+  coefs_2 <- rbind(coefs1_2, coefs_2)
+  coefs_2$grp <- "2020"
+  coefs_2 <- left_join(coefs_2, conf_2, by = "term")
+  
+  coefs <- rbind(coefs_1, coefs_2)
+  
+  ## plot both the 2018-19 and 2020 data
+  ggplot(data = coefs, aes(term, estimate, group = grp))+
+    geom_ribbon(data = coefs, aes(ymin = low, ymax = upper, group = grp, fill = grp), linetype=2, alpha = 0.25) +
+    scale_fill_brewer(palette = "Set1") +
+    geom_point(data = coefs, aes(term, estimate))+ 
+    geom_line(data = coefs, aes(group = grp)) +
+    labs(title = plot_title) +
+    theme(axis.text.x = element_text(angle = 90)) + 
+    theme_bw()
+  
+}
+
+data <- rename(data, xvar = mean_high_c)
+data <- rename(data, yvar = visitors_percap)
+
+## plot binned data for full year
+plot_title <- paste0("Mobility Index v Avg High pre + post 2020")
+plot_data_bin(data, plot_title, xlab = "high_temp (0-40C)")
+
+## plot binned data for 2018/19 summer only 
+plot_title <- paste0("Mobility Index v Avg High Summer pre + post 2020")
+plot_data_bin(data, plot_title, xlab = "high_temp (0-40C)", summer = T)
+
+dev.off()
+
--- figure3.R
+++ figure3.R
@@ -4,9 +4,7 @@ ifelse(dir.exists("~/Box Sync/heatwave_covid"),
 
 source("./regression_functions_shelter.R")
 
-library(dplyr)
 library(lfe)
-library(ggplot2)
 library(dotwhisker)
 library(tidyverse)
 library(lubridate)
@@ -17,97 +15,145 @@ library(gridExtra)
 library(wesanderson)
 library(SafeGraphR)
 library(interactions)
+library(sf)
 
-## read in the regression data
-data <- readRDS("./heatwaves_manual/data_for_regression.rds")
+#### Read in files
+ifelse(dir.exists("~/Box Sync/heatwave_covid/"),
+       setwd("~/Box Sync/heatwave_covid/"),
+       setwd("/oak/stanford/groups/omramom/group_members/aminaly/heatwave_covid/"))
 
-## lets do some plots
-pdf(paste0("./visuals/figure3", Sys.Date(), ".pdf"))
+zoning_cbg <- read_rds("./heatwaves_manual/BayAreaZoning/data/shapefile/zoning_cbg.rds")
+temp_mobility_data <- read_rds("./heatwaves_manual/data_for_regression.rds")
+
+#### prep data
+zoning_cbg$cbg <- paste0(zoning_cbg$STATEFP, zoning_cbg$COUNTYFP, zoning_cbg$TRACTCE, zoning_cbg$BLKGRPCE)
+zoning_cbg_summary <- zoning_cbg %>% filter(zoning %in% c(0:3)) %>% group_by(cbg) %>%
+  summarize(non_res = sum(zoning == 0)/length(zoning), 
+            multi = sum(zoning == 2)/length(zoning), 
+            single = sum(zoning == 1)/length(zoning),
+            non_dev = sum(zoning == 3)/length(zoning))
+
+# version without geometry for calulations across columns
+zoning_cbg_nogeo <- as.data.frame(zoning_cbg_summary)[,1:4]
+zcng_nocbg <- zoning_cbg_nogeo[,2:4]
+
+# calculate which zone type is the dominant and return column name (also populate nogeo)
+zoning_cbg_summary$main_zoning <- colnames(zcng_nocbg)[apply(zcng_nocbg, 1, which.max)]
+zoning_cbg_nogeo$main_zoning <- colnames(zcng_nocbg)[apply(zcng_nocbg, 1, which.max)]
+
+## combine zoning with mobility data
+temp_mobility_data_sm <- temp_mobility_data %>% select(cbg = census_block_group, date, 
+                                                       yvar = visitors_percap, year, 
+                                                       month = month.x, fips)
+zoning_mob <- merge(temp_mobility_data_sm, zoning_cbg_summary, by = "cbg")
+
+## zoning and mobility average for 2020
+temp_mobility_2020 <- temp_mobility_data_sm %>% filter(year == 2020) %>%
+  group_by(cbg) %>% summarise(yvar = mean(yvar, na.rm = T)) %>%
+  mutate(yvar_cut = cut(yvar, breaks = c(seq(1, 3, .5), Inf)))
+zoning_mob_2020 <- merge(temp_mobility_2020, zoning_cbg_summary, by = "cbg")
+
+## zoning and mobility average for summer 2020
+temp_mobility_summer_2020 <- temp_mobility_data_sm %>% 
+  filter(year == 2020 & month %in% c(5:9)) %>%
+  group_by(cbg) %>% summarise(yvar = mean(yvar, na.rm = T))
+zoning_mob_summer_2020 <- merge(temp_mobility_summer_2020, zoning_cbg_summary, by = "cbg")
+
+#### Start PDF
+pdf(paste0("./visuals/pub_figures/fig3_", Sys.time(), ".pdf"))
 ##Finalize datasets for regressions & run
 plot(c(0, 1), c(0, 1), ann = F, bty = 'n', type = 'n', xaxt = 'n', yaxt = 'n',
      main = title)
 text(x = 0.5, y = 0.5, paste(timestamp(), "\n Bay Area Data Overview"),
      cex = 1.5, col = "black")
 
-plot_data_bin <- function(data, plot_title, xlab="Temp (C)", ylab = "# Visitors / Home Devices", summer = F) {
-  
-  #create the bins
-  LVL <- "bin"
-  if(summer) {
-    BINS <- 6
-    data_s <- data %>% filter(between(month.x, 5, 9))
-    data_s <- data_s %>% mutate(xvar_bin = cut(xvar, breaks = c(-Inf, seq(16, 35, 4), Inf), labels = F)) %>% 
-      mutate(xvar_bin = factor(xvar_bin, levels = as.character(1:BINS))) %>% filter(!is.na(xvar_bin))
-  } else {
-    BINS <- 8
-    data_s <- data %>% mutate(xvar_bin = cut(xvar, breaks = c(-Inf, seq(10, 35, 4), Inf), labels = F)) %>% 
-    mutate(xvar_bin = factor(xvar_bin, levels = as.character(1:BINS))) %>% filter(!is.na(xvar_bin))
-  }
-  
-  #separate the two datasets
-  data_1 <- data_s %>% filter(year %in% c(2018,2019))
-  data_2 <- data_s %>% filter(year == 2020)
-  
-  #get the results of the model
-  model_1 <- fe_model(data_1, level = LVL)
-  model_2 <- fe_model(data_2, level = LVL)
-  
-  #bootstrap the data by doing the model however many times, getting the coefficient values (1)
-  boots_1 <- bootstrap_data(data_1, short=T, level= LVL)
-  boots_1 <- boots_1[-1,]
-  boots_1 <- as_tibble(boots_1) %>% mutate(xvar_bin1 = 0) %>% relocate(xvar_bin1)
-  conf_1 <- apply(boots_1,2,function(x) quantile(x,probs=c(0.05,0.5,0.95), na.rm = T)) 
-  conf_1 <- as.data.frame(cbind(1:BINS, conf_1[1,], conf_1[2,], conf_1[3,]))
-  colnames(conf_1) <- c("term", "low", "mid", "upper")   
-  conf_1$term <- row.names(conf_1)
-  
-  coefs_1 <- tidy(model_1, conf.int = T)
-  coefs_1 <- coefs_1[grepl("xvar_bin", coefs_1$term),]
-  coefs1_1 <- coefs_1[1,] %>% mutate(term = "xvar_bin1", estimate = 0, std.error = 0, statistic = 0, p.value = 0, conf.low = 0, conf.high = 0)
-  coefs_1 <- rbind(coefs1_1, coefs_1)
-  coefs_1$grp <- "2018-19"
-  coefs_1 <- left_join(coefs_1, conf_1, by = "term")
-  
-  #bootstrap the data by doing the model however many times, getting the coefficient values (2)
-  boots_2 <- bootstrap_data(data_2, short=T, level= LVL)
-  boots_2 <- boots_2[-1,]
-  boots_2 <- as_tibble(boots_2) %>% mutate(xvar_bin1 = 0) %>% relocate(xvar_bin1)
-  conf_2 <- apply(boots_2,2,function(x) quantile(x,probs=c(0.05,0.5,0.95), na.rm = T)) 
-  conf_2 <- as.data.frame(cbind(1:BINS, conf_2[1,], conf_2[2,], conf_2[3,]))
-  colnames(conf_2) <- c("term", "low", "mid", "upper")   
-  conf_2$term <- row.names(conf_2)
-  
-  coefs_2 <- tidy(model_2, conf.int = T)
-  coefs_2 <- coefs_2[grepl("xvar_bin", coefs_2$term),]
-  coefs1_2 <- coefs_2[1,] %>% mutate(term = "xvar_bin1", estimate = 0, std.error = 0, statistic = 0, p.value = 0, conf.low = 0, conf.high = 0)
-  coefs_2 <- rbind(coefs1_2, coefs_2)
-  coefs_2$grp <- "2020"
-  coefs_2 <- left_join(coefs_2, conf_2, by = "term")
-  
-  coefs <- rbind(coefs_1, coefs_2)
-  
-  ## plot both the 2018-19 and 2020 data
-  ggplot(data = coefs, aes(term, estimate, group = grp))+
-    geom_ribbon(data = coefs, aes(ymin = low, ymax = upper, group = grp, fill = grp), linetype=2, alpha = 0.25) +
-    scale_fill_brewer(palette = "Set1") +
-    geom_point(data = coefs, aes(term, estimate))+ 
-    geom_line(data = coefs, aes(group = grp)) +
-    labs(title = plot_title) +
-    theme(axis.text.x = element_text(angle = 90)) + 
-    theme_bw()
-  
-}
+#### Plot zoning allocations
+ggplot(data = zoning_cbg_summary) +
+  ggtitle("Main Zoning per CBG") +
+  geom_sf(data = zoning_cbg_summary, aes(fill = main_zoning), color = NA) +
+  scale_color_manual(values=wes_palette(n=4, name="GrandBudapest1"), na.value = "grey") +
+  labs(colour="Zoning") +
+  theme_bw()
 
-data <- rename(data, xvar = mean_high_c)
-data <- rename(data, yvar = visitors_percap)
+#plot zoning in specific counties
+#santa clara
+santaclara <- zoning_cbg_summary %>% mutate(fips = substr(cbg, 1, 5)) %>%
+  filter(fips == "06085")
+ggplot(data = santaclara) +
+  ggtitle("Main Zoning per CBG") +
+  geom_sf(data = santaclara, aes(fill = main_zoning), color = NA) +
+  scale_color_manual(values=wes_palette(n=4, name="GrandBudapest1"), na.value = "grey") +
+  labs(colour="Zoning") +
+  theme_bw()
 
-## plot binned data for full year
-plot_title <- paste0("Mobility Index v Avg High pre + post 2020")
-plot_data_bin(data, plot_title, xlab = "high_temp (0-40C)")
+#san francisco
+sf <- zoning_cbg_summary %>% mutate(fips = substr(cbg, 1, 5)) %>%
+  filter(fips == "06075")
+ggplot(data = sf) +
+  ggtitle("Main Zoning per CBG") +
+  geom_sf(data = sf, aes(fill = main_zoning), color = NA) +
+  scale_color_manual(values=wes_palette(n=4, name="GrandBudapest1"), na.value = "grey") +
+  labs(colour="Zoning") +
+  theme_bw()
 
-## plot binned data for 2018/19 summer only 
-plot_title <- paste0("Mobility Index v Avg High Summer pre + post 2020")
-plot_data_bin(data, plot_title, xlab = "high_temp (0-40C)", summer = T)
+#alameda
+alameda <- zoning_cbg_summary %>% mutate(fips = substr(cbg, 1, 5)) %>%
+  filter(fips == "06001")
+ggplot(data = alameda) +
+  ggtitle("Main Zoning per CBG") +
+  geom_sf(data = alameda, aes(fill = main_zoning), color = NA) +
+  scale_color_manual(values=wes_palette(n=4, name="GrandBudapest1"), na.value = "grey") +
+  labs(colour="Zoning") +
+  theme_bw()
 
+#### Plot 2020 mobility for each zone in Santa Clara County
+santaclara <- zoning_mob_2020 %>% mutate(fips = substr(cbg, 1, 5)) %>%
+  filter(fips == "06085") 
+
+ggplot(data = santaclara) +
+  ggtitle("Mobility Santa Clara County") +
+  geom_sf(data = santaclara, size = 0.002, aes(fill = yvar_cut, geometry = geometry), color = NA) +
+  scale_color_gradient(low = "#31a354", high = "#c51b8a", na.value = "grey") +
+  facet_wrap( ~ main_zoning, nrow = 2) +
+  labs(colour="Zoning") +
+  theme_bw()
+
+#### Plot 2020 mobility for each zone in SF County
+sf <- zoning_mob_2020 %>% mutate(fips = substr(cbg, 1, 5)) %>%
+  filter(fips == "06075") 
+print("I got to sf ")
+
+ggplot(data = sf) +
+  ggtitle("Mobility San Francisco County") +
+  geom_sf(data = santaclara, aes(fill = yvar_cut, geometry = geometry), color = NA) +
+  scale_color_manual(values=wes_palette(n=4, name="GrandBudapest1"), na.value = "grey") +
+  facet_wrap( ~ main_zoning, nrow = 2) +
+  labs(colour="Zoning") +
+  theme_bw()
+
+#### Plot 2020 mobility for each zone in Alameda County
+alameda <- zoning_mob_2020 %>% mutate(fips = substr(cbg, 1, 5)) %>%
+  filter(fips == "06001")
+
+ggplot(data = alameda) +
+  ggtitle("Mobility Alameda County") +
+  geom_sf(data = santaclara, aes(fill = yvar_cut, geometry = geometry), color = NA) +
+  scale_color_manual(values=wes_palette(n=4, name="GrandBudapest1"), na.value = "grey") +
+  facet_wrap( ~ main_zoning, nrow = 2) +
+  labs(colour="Zoning") +
+  theme_bw()
+
+
+##over time
+# # line plot of mobility visitors over time separated by main zoning, grouped by year
+ggplot(data=zoning_mob, aes(x=date, y=yvar, group=main_zoning)) +
+  geom_smooth(aes(group=main_zoning, color=as.factor(main_zoning))) +
+  ggtitle("Mobility by Zoning") + ylab("# Visitors / Home Devices") + xlab("Date") +
+  scale_color_manual(values=wes_palette(n=4, name="GrandBudapest1")) +
+  scale_x_date() +
+  theme(text = element_text(size = 15)) +
+  labs(colour="Zoning") +
+  theme_bw()
+
+#### Shut down pdf
 dev.off()
-
