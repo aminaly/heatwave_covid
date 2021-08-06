7a379fc8bae2c93c4d3f9100baf4120fc3a3bbdd
Amina Ly <aminaly@SE3-C02W3433HV29.local>
2021-08-06 16:50:41 -0400

    grouping name

--- figure3.R
+++ figure3.R
@@ -47,7 +47,7 @@ zoning_cbg_nogeo$main_zoning <- colnames(zcng_nocbg)[apply(zcng_nocbg, 1, which.
 ## combine zoning with mobility data
 temp_mobility_data_sm <- temp_mobility_data %>% 
   filter(mean_high_c >= 34) %>%
-  group_by(year, cbg, fips, xvar = mean_high_c) %>%
+  group_by(year, cbg = census_block_group, fips, xvar = mean_high_c) %>%
   summarize(yvar = mean(visitors_percap, na.rm = T))
 
 zoning_mob <- merge(temp_mobility_data_sm, zoning_cbg_summary, by = c("cbg", "fips"))
@@ -70,8 +70,8 @@ zoning_mob_over30_2020 <- zoning_mob %>% filter(year == 2020)
 ggplot(data = zoning_mob_over30_2018) +
   ggtitle("Bay Area 2018 Summer Mobility Over 30 Degrees") +
   geom_sf(data = zoning_mob_over30_2018, size = 0.002, aes(fill = yvar, geometry = "geometry")) +
-  #scale_fill_brewer(palette = "PiYG", direction = -1, na.value = "grey") +
-  #labs(colour="Mobility Metric") +
+  scale_fill_brewer(palette = "PiYG", direction = -1, na.value = "grey") +
+  labs(colour="Mobility Metric") +
   theme_bw()
 
 ggplot(data = zoning_mob_over30_2019) +
