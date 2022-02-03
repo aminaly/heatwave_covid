e2dc13b7fce433b042f5d166bd1c6ccaa5a8f59b
Amina Ly <amina.ly626@gmail.com>
2022-02-03 14:10:20 -0800

    3 separate years

--- figure2.R
+++ figure2.R
@@ -98,16 +98,103 @@ plot_data_bin <- function(data, plot_title, xlab="Temp (C)", ylab = "# Visitors 
   
 }
 
+plot_data_bin3 <- function(data, plot_title, xlab="Temp (C)", ylab = "# Visitors / Home Devices", summer = F) {
+  
+  #create the bins
+  LVL <- "bin"
+  if(summer) {
+    BINS <- 7
+    data_s <- data %>% filter(between(month.x, 5, 9))
+    data_s <- data_s %>% mutate(xvar_bin = cut(xvar, breaks = c(-Inf, seq(16, 39, 4), Inf), labels = F)) %>% 
+      mutate(xvar_bin = factor(xvar_bin, levels = as.character(1:BINS))) %>% filter(!is.na(xvar_bin))
+  } else {
+    BINS <- 8
+    data_s <- data %>% mutate(xvar_bin = cut(xvar, breaks = c(-Inf, seq(10, 35, 4), Inf), labels = F)) %>% 
+      mutate(xvar_bin = factor(xvar_bin, levels = as.character(1:BINS))) %>% filter(!is.na(xvar_bin))
+  }
+  
+  #separate the three datasets
+  data_1 <- data_s %>% filter(year == 2018)
+  data_2 <- data_s %>% filter(year == 2019)
+  data_3 <- data_s %>% filter(year == 2020)
+  
+  #get the results of the model
+  model_1 <- fe_model(data_1, level = LVL)
+  model_2 <- fe_model(data_2, level = LVL)
+  model_3 <- fe_model(data_3, level = LVL)
+  
+  #bootstrap the data by doing the model however many times, getting the coefficient values (1)
+  boots_1 <- bootstrap_data(data_1, short=T, level= LVL)
+  boots_1 <- boots_1[-1,]
+  boots_1 <- as_tibble(boots_1) %>% mutate(xvar_bin1 = 0) %>% relocate(xvar_bin1)
+  conf_1 <- apply(boots_1,2,function(x) quantile(x,probs=c(0.05,0.5,0.95), na.rm = T)) 
+  conf_1 <- as.data.frame(cbind(1:BINS, conf_1[1,], conf_1[2,], conf_1[3,]))
+  colnames(conf_1) <- c("term", "low", "mid", "upper")   
+  conf_1$term <- row.names(conf_1)
+  
+  coefs_1 <- tidy(model_1, conf.int = T)
+  coefs_1 <- coefs_1[grepl("xvar_bin", coefs_1$term),]
+  coefs1_1 <- coefs_1[1,] %>% mutate(term = "xvar_bin1", estimate = 0, std.error = 0, statistic = 0, p.value = 0, conf.low = 0, conf.high = 0)
+  coefs_1 <- rbind(coefs1_1, coefs_1)
+  coefs_1$grp <- "2018-19"
+  coefs_1 <- left_join(coefs_1, conf_1, by = "term")
+  
+  #bootstrap the data by doing the model however many times, getting the coefficient values (2)
+  boots_2 <- bootstrap_data(data_2, short=T, level= LVL)
+  boots_2 <- boots_2[-1,]
+  boots_2 <- as_tibble(boots_2) %>% mutate(xvar_bin1 = 0) %>% relocate(xvar_bin1)
+  conf_2 <- apply(boots_2,2,function(x) quantile(x,probs=c(0.05,0.5,0.95), na.rm = T)) 
+  conf_2 <- as.data.frame(cbind(1:BINS, conf_2[1,], conf_2[2,], conf_2[3,]))
+  colnames(conf_2) <- c("term", "low", "mid", "upper")   
+  conf_2$term <- row.names(conf_2)
+  
+  coefs_2 <- tidy(model_2, conf.int = T)
+  coefs_2 <- coefs_2[grepl("xvar_bin", coefs_2$term),]
+  coefs1_2 <- coefs_2[1,] %>% mutate(term = "xvar_bin1", estimate = 0, std.error = 0, statistic = 0, p.value = 0, conf.low = 0, conf.high = 0)
+  coefs_2 <- rbind(coefs1_2, coefs_2)
+  coefs_2$grp <- "2020"
+  coefs_2 <- left_join(coefs_2, conf_2, by = "term")
+  
+  #bootstrap the data by doing the model however many times, getting the coefficient values (3)
+  boots_3 <- bootstrap_data(data_3, short=T, level= LVL)
+  boots_3 <- boots_3[-1,]
+  boots_3 <- as_tibble(boots_3) %>% mutate(xvar_bin1 = 0) %>% relocate(xvar_bin1)
+  conf_3 <- apply(boots_3,2,function(x) quantile(x,probs=c(0.05,0.5,0.95), na.rm = T)) 
+  conf_3 <- as.data.frame(cbind(1:BINS, conf_3[1,], conf_3[2,], conf_3[3,]))
+  colnames(conf_3) <- c("term", "low", "mid", "upper")   
+  conf_3$term <- row.names(conf_3)
+  
+  coefs_3 <- tidy(model_3, conf.int = T)
+  coefs_3 <- coefs_3[grepl("xvar_bin", coefs_3$term),]
+  coefs1_3 <- coefs_3[1,] %>% mutate(term = "xvar_bin1", estimate = 0, std.error = 0, statistic = 0, p.value = 0, conf.low = 0, conf.high = 0)
+  coefs_3 <- rbind(coefs1_3, coefs_3)
+  coefs_3$grp <- "2020"
+  coefs_3 <- left_join(coefs_3, conf_3, by = "term")
+  
+  coefs <- rbind(coefs_1, coefs_2, coefs_3)
+  
+  ## plot both the 2018-19 and 2020 data
+  ggplot(data = coefs, aes(term, estimate, group = grp))+
+    geom_ribbon(data = coefs, aes(ymin = low, ymax = upper, group = grp, fill = grp), linetype=2, alpha = 0.25) +
+    scale_fill_brewer(palette = "Set1") +
+    geom_point(data = coefs, aes(term, estimate))+ 
+    geom_line(data = coefs, aes(group = grp)) +
+    labs(title = plot_title) +
+    theme(axis.text.x = element_text(angle = 90)) + 
+    theme_bw()
+  
+}
+
 data <- rename(data, xvar = mean_high_c)
 data <- rename(data, yvar = visitors_percap)
 
 ## plot binned data for full year
 plot_title <- paste0("Mobility Index v Avg High pre + post 2020")
-plot_data_bin(data, plot_title, xlab = "high_temp (0-40C)")
+plot_data_bin3(data, plot_title, xlab = "high_temp (0-40C)")
 
 ## plot binned data summer only 
 plot_title <- paste0("Mobility Index v Avg High Summer pre + post 2020")
-plot_data_bin(data, plot_title, xlab = "high_temp (0-40C)", summer = T)
+plot_data_bin3(data, plot_title, xlab = "high_temp (0-40C)", summer = T)
 
 
 dev.off()
