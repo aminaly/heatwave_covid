eacf09cd94f50fcafc89969100d5cd8ab447f502
Amina Ly <amina.ly626@gmail.com>
2022-02-02 19:43:23 -0800

    new visuals

--- batch_files/run_extract.sh
+++ batch_files/run_extract.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=extractTemps
 #SBATCH --nodes=1
-#SBATCH --array=71-86
+#SBATCH --array=43,86
 #SBATCH --error=/oak/stanford/groups/omramom/group_members/aminaly/heatwave_covid/outfiles/extractTemps.err
 #SBATCH --output=/oak/stanford/groups/omramom/group_members/aminaly/heatwave_covid/outfiles/extractTemps.out
 #SBATCH --time=48:00:00
@@ -13,6 +13,7 @@
 
 ml physics gdal udunits/2.2.26 netcdf/4.4.1.1 R/3.6.1 proj geos;
 module load R/3.6.1
+ml curl/7.81.0
 
 cd $OAK/group_members/aminaly/heatwave_covid
 let buffer=$SLURM_ARRAY_TASK_ID
