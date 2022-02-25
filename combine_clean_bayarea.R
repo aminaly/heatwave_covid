3e914b016805c7b46a5b05c634296bb2c83a67bb
Amina Ly <amina.ly626@gmail.com>
2022-02-25 15:41:50 -0800

    ?

--- combine_clean_bayarea.R
+++ combine_clean_bayarea.R
@@ -41,8 +41,6 @@ if(RUNTEMP) {
     f <- f %>% mutate(census_block_group = as.character(f$blockgroup)) %>% select(-fips) %>%
       mutate(census_block_group = ifelse(nchar(census_block_group) == 11, 
                                          paste0("0", census_block_group), census_block_group))
-    f$fips <- str_sub(f$census_block_group, 1,5)
-    f <- f %>% filter(fips %in% included_fips)
     temps <- bind_rows(temps, f)
     print(head(f))
     
@@ -163,8 +161,7 @@ pop_income <- left_join(income, pops, by = c("census_block_group", "fips"))
 #### Combine all data ----
 
 ## combine movement and home 
-patterns <- left_join(movement, home, by = c("census_block_group", "year", "month"))
-patterns <- patterns %>% rename(fips = fips.x)
+patterns <- left_join(movement, home, by = c("census_block_group", "fips", "year", "month"))
 
 ## add in income
 print(head(patterns))
